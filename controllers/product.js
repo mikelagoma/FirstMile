64a9fe24d72ce74fbb3e0dd5d6f87ccf130b2979
M L <mike@Ms-MacBook-Pro.routerlogin.net>
2019-07-27 09:07:49 -0500

    search page

--- controllers/product.js
+++ controllers/product.js
@@ -1,10 +1,29 @@
+//TODO: create model and add product pages
+Products = Array()
+
+for (var i=0; i<11; i++) {
+  product = {
+    "id": i,
+    "title": "Kayak " + i,
+    "description": "an expensive kayak",
+    "img": "images/kayak" + i + ".jpg",
+    "status": "available",
+    "price": 50,
+    "owner": "5d3a787df38d26a1b050c9e5",
+    "renter": null
+  }
+  Products.push(product)
+}
+
 /**
  * GET /
  * Rental product search page.
  */
 exports.search = (req, res) => {
-	console.log(JSON.stringify(req.user))
+	console.log(JSON.stringify(Products))
+  console.log(Products[0].title)
   res.render('search', {
-    title: 'Home'
+    title: 'Search',
+    products: Products
   });
 };
